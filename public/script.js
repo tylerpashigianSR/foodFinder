5d00a86f2a3c7dfd9c8a7149a555bdc88480fb1b
Alex Olivero <aoliv1@sapient.com>
2017-07-19 12:57:54 -0500

    displays 20 nearest restaurants to the merchandise mart

---
+++ public/script.js
@@ -0,0 +1,32 @@
+var foodFinder = angular.module('foodFinder', ['ngRoute']);
+
+   // configure our routes
+   foodFinder.config(function($routeProvider) {
+       $routeProvider
+
+           // route for the home page
+           .when('/', {
+               templateUrl : 'home.html',
+               controller  : 'mainController'
+           })
+
+           // route for the about page
+           .when('/about', {
+               templateUrl : 'about.html',
+               controller  : 'aboutController'
+           })
+   });
+
+   // create the controller and inject Angular's $scope
+   foodFinder.controller('mainController', function($scope, $http) {
+       // create a message to display in our view
+       $scope.message = 'Everyone come and see how good I look!';
+       $scope.places = [];
+
+       $http.get("http://localhost:3000/places")
+           .success(function (response){
+             console.log(response);
+              $scope.places = response;
+           });
+
+   });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/places', function(req,res){
+  //location is hardcoded to Merchandise Mart
+  request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.8885,-87.6354&type=restaurant&rankby=distance&key=AIzaSyCXyqmOpSzVX0R85aM-p7jEHKU1SPD1_TQ",
+   function(error, response, body) {
+     var places = JSON.parse(body).results;
+     res.send(places);
+  });
 });
 
 module.exports = router;
