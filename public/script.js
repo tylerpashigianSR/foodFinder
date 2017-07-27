53d1b654dde406a748f0e3148780ec1ac0638731
Alex Olivero <aoliv1@sapient.com>
2017-07-27 16:31:39 -0500

    a lot of email stuff and group confirmation. when a group of 4 is assembled for a restaurant, that group is created, they all get emails, they're taken off the lists for every restaurant, and when they go to the website with that email they will see their group&restaurant details instead of the restaurant search

--- public/script.js
+++ public/script.js
@@ -28,19 +28,34 @@ var foodFinder = angular.module('foodFinder', ['ngRoute']);
        $scope.max_price = 3;
        $scope.keyword = null;
        $scope.validated = false;
+       $scope.finalResults = {place:null,emails:null};
 
        $scope.submitEmail = function(){
          if($scope.email != ""){
            $scope.emailLockedIn = !$scope.emailLockedIn;
          }
          if($scope.emailLockedIn){
-          init();
+          $scope.finalResults = {place:null,emails:null};
+          checkForGroup();
           $(document).ready(function () {
               $("html, body").animate({
                   scrollTop: $("#search").offset().top
               }, 1500);
           });
          }
+       }
+
+       var checkForGroup = function(){
+         $http.get("http://localhost:3000/checkForGroup/" + $scope.email)
+             .success(function (response){
+                if(response.place == null){
+                  $scope.searchForPlaces(false);
+                }
+                else{
+                  $scope.finalResults.place = response.place;
+                  $scope.finalResults.emails = response.emails;
+                }
+             });
        }
 
        $scope.searchForPlaces = function(showMoreClicked){
@@ -69,23 +84,23 @@ var foodFinder = angular.module('foodFinder', ['ngRoute']);
        }
 
        $scope.add = function(index, place) {
-         $http.get('/increaseCounter/' + place.id + '/' + $scope.email)
+         $http.get('/increaseCounter/' + place.place_id + '/' + $scope.email)
              .success(function (response){
                place.counter++;
                place.chosen = true;
+               if(response.place != null){
+                 $scope.finalResults.place = response.place;
+                 $scope.finalResults.emails = response.emails;
+               }
              });
        }
 
        $scope.remove = function(index, place) {
-         $http.get('/decreaseCounter/' + place.id + '/' + $scope.email)
+         $http.get('/decreaseCounter/' + place.place_id + '/' + $scope.email)
              .success(function (response){
                place.counter--;
                place.chosen = false;
              });
-       }
-
-       init = function(){
-         $scope.searchForPlaces(false);
        }
 
    });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'munchmatesalerts@gmail.com',
+    pass: 'munchmates123'
+  }
+});
+const groupMatchMinimum = 4;
 var chosenRestaurants = {};
+var formedGroups = []; //consist of lists of key:place_id, values: emails
+
+router.get('/checkForGroup/:email', function(req,res){
+  var alreadyInFormedGroup = false;
+  for(var i = 0; i < formedGroups.length; i++){
+    var index = formedGroups[i].emails.indexOf(req.params.email);
+    if(index >= 0){
+      alreadyInFormedGroup = true;
+      request("https://maps.googleapis.com/maps/api/place/details/json?placeid="
+        + formedGroups[i].place_id + "&key=AIzaSyCXyqmOpSzVX0R85aM-p7jEHKU1SPD1_TQ",
+        function(error, response, body) {
+          res.send({place:response,emails:formedGroups[i].emails});
+      });
+      break;
+    }
+  }
+  if(!alreadyInFormedGroup){
+    res.send({place:null,emails:null});
+  }
+});
 
 router.get('/places/:keyword/:max_price/:email', function(req,res){
   var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.8885,-87.6354&rankby=distance&type=restaurant&opennow=true&maxprice=" +
@@ -28,43 +56,89 @@ router.get('/morePlaces/:next_page_token/:email', function(req,res){
     });
 });
 
-router.get('/increaseCounter/:id/:email', function(req, res) {
-  if (chosenRestaurants[req.params.id] == null) {
-    chosenRestaurants[req.params.id] = [req.params.email];
-  } else {
-    if(chosenRestaurants[req.params.id].indexOf(req.params.email) < 0){
-      chosenRestaurants[req.params.id].push(req.params.email);
+router.get('/increaseCounter/:place_id/:email', function(req, res) {
+  var groupFound = false;
+  if (chosenRestaurants[req.params.place_id] == null) {
+    chosenRestaurants[req.params.place_id] = [req.params.email];
+  }
+  else {
+    if(chosenRestaurants[req.params.place_id].indexOf(req.params.email) < 0){
+      //user's email is not in the list for this restaurant
+      chosenRestaurants[req.params.place_id].push(req.params.email);
+      //now check to see if a group has been filled
+      if(chosenRestaurants[req.params.place_id].length >= groupMatchMinimum){
+
+        groupFound = true;
+        var emails = chosenRestaurants[req.params.place_id];
+        chosenRestaurants[req.params.place_id] = [];
+        removeEmailsFromAllLists(emails);
+
+        //send emails
+        var mailOptions = {
+          from: 'munchmatesalerts@gmail.com',
+          to: emails.join(),
+          subject: 'Your MunchMates have been found!',
+          text: 'That was easy!'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+
+        formedGroups.push({place_id:req.params.place_id,emails:emails});
+
+        request("https://maps.googleapis.com/maps/api/place/details/json?placeid="
+          + req.params.place_id + "&key=AIzaSyCXyqmOpSzVX0R85aM-p7jEHKU1SPD1_TQ",
+          function(error, response, body) {
+            res.send({place:response,emails:emails});
+        });
+      }
     }
   }
+  if(!groupFound){
+    res.send({place_id:null,emails:null});
+  }
   console.log(chosenRestaurants);
-  res.send("all good");
 });
 
-router.get('/decreaseCounter/:id/:email', function(req, res) {
-
-  if (chosenRestaurants[req.params.id] != null) {
-    var index = chosenRestaurants[req.params.id].indexOf(req.params.email);
+router.get('/decreaseCounter/:place_id/:email', function(req, res) {
+  if (chosenRestaurants[req.params.place_id] != null) {
+    var index = chosenRestaurants[req.params.place_id].indexOf(req.params.email);
     if(index >= 0){
-      chosenRestaurants[req.params.id].splice(index, 1);
+      chosenRestaurants[req.params.place_id].splice(index, 1);
     }
   }
   console.log(chosenRestaurants);
   res.send("removed. all good");
 });
 
+function removeEmailsFromAllLists(emails){
+  for (var key in chosenRestaurants) { //look at each restaurant
+    for(var i = 0; i < emails.length; i++){ //for each email to be removed
+      var index = chosenRestaurants[key].indexOf(emails[i]);
+      if(index >= 0){
+        chosenRestaurants[req.params.place_id].splice(index, 1);
+      }
+    }
+  }
+}
+
 function processResults(body, email){
     var results = JSON.parse(body).results;
     for(var i = 0; i < results.length; i++){
        results[i].distanceFromOffice = distance(41.8885,-87.6354,results[i].geometry.location.lat,results[i].geometry.location.lng);
        results[i].dollarSigns = priceLevelToDollarSigns(results[i].price_level);
-       if(chosenRestaurants[results[i].id] == null){
+       if(chosenRestaurants[results[i].place_id] == null){
           results[i].counter = 0;
           results[i].chosen = false;
        }
        else{
-          results[i].counter = chosenRestaurants[results[i].id].length;
-          var index = chosenRestaurants[results[i].id].indexOf(email);
-          if(index >= 0){
+          results[i].counter = chosenRestaurants[results[i].place_id].length;
+          if(chosenRestaurants[results[i].place_id].indexOf(email) >= 0){
             results[i].chosen = true;
           }
        }
